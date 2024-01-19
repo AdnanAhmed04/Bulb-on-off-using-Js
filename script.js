ab22f66c68fa2ad49e4752482313d619cf2bf0d9
AdnanAhmed <127620300+AdnanAhmed04@users.noreply.github.com>
2024-01-19 23:30:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeImage1() {
+    console.log('Changing image...');
+    var imgElement = document.getElementById('myImage');
+    imgElement.src = 'https://www.w3schools.com/html/pic_bulbon.gif'; // Replace 'new_image_url.jpg' with the URL of the new image
+}
+
+function changeImage2() {
+    console.log('Changing image...');
+    var imgElement = document.getElementById('myImage');
+    imgElement.src = 'https://www.w3schools.com/html/pic_bulbon.gif'; // Replace 'new_image_url.jpg' with the URL of the new image
+}
